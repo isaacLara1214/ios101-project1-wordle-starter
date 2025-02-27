7dd0fba1c3896acdec4a412902b6de55468c08ef
Isaac Lara <isaac.lara014@gmail.com>
2025-02-27 01:57:13 -0500

    added launch screen as well as app icon

--- Wordle/WordGenerator.swift
+++ Wordle/WordGenerator.swift
@@ -12,7 +12,7 @@ class WordGenerator {
   // IMPORTANT: MAKE SURE ALL THE LETTERS IN THE NEW WORDS YOU ADD ARE CAPITALIZED (e.g. "AUDIO" instead of "audio")
   // Tip: Look at the Array API documentation to see what method returns a random element
   // Checkpoint: After finishing this exercise, you should now have a different goal word each time you run the app! Try printing out the selected goal word to check. If it's not working, check that you have done this exercise correctly.
-  static let possibleWords = ["ABCDE", "AUDIO", "AINME", "ELBOW", "STARE", "TANGY"]
+  static let possibleWords = ["ABCDE", "AUDIO", "ANIME", "ELBOW", "STARE", "TANGY"]
   static func generateRandomWord() -> String? {
     // START YOUR CODE HERE
       let goalWord = possibleWords.randomElement()
